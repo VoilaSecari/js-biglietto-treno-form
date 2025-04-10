2234dd486171dadcbecd655fc90e77e88e3e9680
VoilaSecari <lifeaslaurafilip@gmail.com>
2025-04-10 19:01:10 +0200

    logica ok da fare la sostituzione inner text in DOM

--- main.js
+++ main.js
@@ -1,33 +1,37 @@
-// console.log(nomeUtente);
+// COSTANTI
+const prezzoKm = 0.21;
+const sconto20 = 20;
+const sconto40 = 40;
 
 const btnEl = document.getElementById("btn-calcolo");
-
-// let isButtonClicked = false;
-// btnEl.addEventListener("click", function () {
-//   if (!isButtonClicked) {
-//     isButtonClicked = true;
-//     console.log("Funziona");
-//   } else {
-//     isButtonClicked = false;
-//     console.log("Funziona 2");
-//   }
-// });
 
 let isButtonClicked = false;
 btnEl.addEventListener("click", function () {
   if (!isButtonClicked) {
+    //BOOLEANO
     isButtonClicked = true;
+    // COSTANTI INPUT
     const nomeUtente = document.querySelector("#nome-utente").value;
     const cognomeUtente = document.querySelector("#cognome-utente").value;
     const anniUtente = parseInt(document.querySelector("#anni-utente").value);
-    console.log(
-      "Nome utente: " +
-        nomeUtente +
-        "\nCognome utente: " +
-        cognomeUtente +
-        "\nEtà utente: " +
-        anniUtente
+    const percorsoUtente = parseInt(
+      document.querySelector("#percorso-utente").value
     );
+    // CALCOLI
+    let prezzoBigliettoNoSconto = percorsoUtente * prezzoKm;
+    let x = prezzoBigliettoNoSconto;
+
+    console.log(nomeUtente, cognomeUtente, anniUtente);
+    // SCONTO 20
+    if (anniUtente < 18) {
+      const bigliettoOk = x - sconto20 * (x / 100) + "€";
+      console.log(bigliettoOk);
+    } else if (anniUtente > 65) {
+      const bigliettoOk = x - sconto40 * (x / 100) + "€";
+      console.log(bigliettoOk);
+    } else {
+      console.log(prezzoBigliettoNoSconto + "€");
+    }
   } else {
     isButtonClicked = false;
     console.log("Cancella dato");
